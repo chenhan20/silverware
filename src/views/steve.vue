f63d052b49d3899f02d166d41553cdc487309ffd
SteveChuang <a0911558945@gmail.com>
2019-08-25 00:20:02 +0800

    add pug、sass loader

--- src/views/steve.vue
+++ src/views/steve.vue
@@ -1,10 +1,27 @@
-<template>
-  <div class="steve">
-      <h1>QQQQQQQQQ</h1>
-  </div>
+<template lang="pug">
+  .steve
+    h1 QQQQQQQQQ
+    a 123
 </template>
 
 <script>
 
 // @ is an alias to /src
 </script>
+
+<style lang="sass">
+$pc-media: 960px
+$phones-media: 480px
+//電腦
+@mixin pc-width() 
+  @media all and (min-width: $pc-media) 
+    @content
+//手機
+@mixin phone-width() 
+  @media all and (max-width: $phones-media) 
+    @content
+
+body
+  background: black
+
+</style>
